332d7e46a36a5e629dd4b0b50be09ab63db0ae08
Aaron O'Leary <aaron.oleary@gmail.com>
2014-09-19 14:20:23 +0100

    namespace the harminv functions

--- harminv.pyx
+++ harminv.pyx
@@ -2,7 +2,7 @@
 import numpy as np
 cimport numpy as np
 
-cimport charminv as charm
+cimport charminv
 
 cdef class Harminv:
     cdef int n
@@ -10,7 +10,7 @@ cdef class Harminv:
     cdef double fmin
     cdef double fmax
     cdef int nf
-    cdef charm.harminv_data data
+    cdef charminv.data data
 
     def __init__(self, n, signal, fmin, fmax, nf):
         self.n = n
@@ -21,30 +21,30 @@ cdef class Harminv:
 
         self.data = self.create_data()
 
-    cdef charm.harminv_data create_data(self):
-        return charm.harminv_data_create(self.n, <double complex *> self.signal.data,
-                                   self.fmin, self.fmax, self.nf)
+    cdef charminv.data create_data(self):
+        return charminv.data_create(self.n, <double complex *> self.signal.data,
+                                    self.fmin, self.fmax, self.nf)
 
     cdef void solve(self):
-        charm.harminv_solve(self.data)
+        charminv.solve(self.data)
 
     cdef int get_num_freqs(self):
-        return charm.harminv_get_num_freqs(self.data)
+        return charminv.get_num_freqs(self.data)
 
     cdef double get_freq(self, int k):
-        return charm.harminv_get_freq(self.data, k)
+        return charminv.get_freq(self.data, k)
 
     cdef double get_Q(self, int k):
-        return charm.harminv_get_Q(self.data, k)
+        return charminv.get_Q(self.data, k)
 
     cdef double get_decay(self, int k):
-        return charm.harminv_get_decay(self.data, k)
+        return charminv.get_decay(self.data, k)
 
     cdef double complex get_omega(self, int k):
-        return charm.harminv_get_omega(self.data, k)
+        return charminv.get_omega(self.data, k)
 
     cdef double complex get_amplitude(self, int k):
-        return charm.harminv_get_amplitude(self.data, k)
+        return charminv.get_amplitude(self.data, k)
 
     cdef double complex get_freq_error(self, int k):
-        return charm.harminv_get_freq_error(self.data, k)
+        return charminv.get_freq_error(self.data, k)
