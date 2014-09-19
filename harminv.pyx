5af30d386a647d63ca219d0d848bdcd1f6555af9
Aaron O'Leary <aaron.oleary@gmail.com>
2014-09-19 14:44:20 +0100

    make functions python usable

--- harminv.pyx
+++ harminv.pyx
@@ -5,11 +5,11 @@ cimport numpy as np
 cimport charminv
 
 cdef class Harminv:
-    cdef int n
-    cdef np.ndarray signal
-    cdef double fmin
-    cdef double fmax
-    cdef int nf
+    cdef readonly int n
+    cdef readonly np.ndarray signal
+    cdef readonly double fmin
+    cdef readonly double fmax
+    cdef readonly int nf
     cdef charminv.data data
 
     def __init__(self, n, signal, fmin, fmax, nf):
@@ -25,26 +25,26 @@ cdef class Harminv:
         return charminv.data_create(self.n, <double complex *> self.signal.data,
                                     self.fmin, self.fmax, self.nf)
 
-    cdef void solve(self):
+    cpdef solve(self):
         charminv.solve(self.data)
 
-    cdef int get_num_freqs(self):
+    cpdef get_num_freqs(self):
         return charminv.get_num_freqs(self.data)
 
-    cdef double get_freq(self, int k):
+    cpdef get_freq(self, int k):
         return charminv.get_freq(self.data, k)
 
-    cdef double get_Q(self, int k):
+    cpdef get_Q(self, int k):
         return charminv.get_Q(self.data, k)
 
-    cdef double get_decay(self, int k):
+    cpdef get_decay(self, int k):
         return charminv.get_decay(self.data, k)
 
-    cdef double complex get_omega(self, int k):
+    cpdef complex get_omega(self, int k):
         return charminv.get_omega(self.data, k)
 
-    cdef double complex get_amplitude(self, int k):
+    cpdef complex get_amplitude(self, int k):
         return charminv.get_amplitude(self.data, k)
 
-    cdef double complex get_freq_error(self, int k):
+    cpdef complex get_freq_error(self, int k):
         return charminv.get_freq_error(self.data, k)
--- test.py
+++ test.py
@@ -1,3 +1,22 @@
+import numpy as np
+
 import harminv
 
 print dir(harminv)
+
+time = np.arange(1000)
+signal = np.cos(time) + np.cos(3*time)
+
+harm = harminv.Harminv(n=signal.size, signal=signal, fmin=0.01, fmax=100, nf=3)
+
+print dir(harm)
+print harm.n
+print harm.fmin
+print harm.fmax
+print harm.nf
+harm.solve()
+
+print harm.get_num_freqs()
+print harm.get_freq(0)
+print harm.get_freq(1)
+print harm.get_freq(2)
