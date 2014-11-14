6ddc80ec3eedc68f9e0ee791b382b333e0d714db
Aaron O'Leary <aaron.oleary@gmail.com>
2014-11-14 16:11:25 +0000

    setup info

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
+    name='harminv',
+    version='0.1',
+    description='Python interface to harminv',
+    py_modules=['harminv'],
+    author="Aaron O'Leary",
+    install_requires=['numpy', 'cython'],
     ext_modules=cythonize([Extension('harminv',
                                      ["harminv.pyx"],
                                      libraries=['harminv'])])
