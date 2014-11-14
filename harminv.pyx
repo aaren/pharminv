e651849403f4e401de58b28991e290c117bf35c3
Aaron O'Leary <aaron.oleary@gmail.com>
2014-11-14 15:30:26 +0000

    api v1

--- harminv.pyx
+++ harminv.pyx
@@ -12,9 +12,34 @@ cdef class Harminv:
     cdef readonly int nf
     cdef charminv.data data
 
-    cdef public double [:] freq
+    cdef public np.ndarray freq
+    cdef public np.ndarray Q
+    cdef public np.ndarray decay
+    cdef public np.ndarray omega
+    cdef public np.ndarray amplitude
+    cdef public np.ndarray freq_error
 
     def __init__(self, signal, fmin, fmax, nf):
+        """Perform Harmonic Inversion on the given signal.
+
+        Assuming that the signal is comprised of a sum of sinusoids
+        (i.e.  modes), extract their frequencies, amplitudes and
+        decay rates (for exponentially decaying sinusoids).
+
+        Sets these attributes:
+
+            freq - the mode frequencies
+            decay - the exponential decay constants
+            Q - the decay lifetime (as in Q-factor)
+            amplitude - the (real, positive) amplitudes
+            omega - the complex angular frequencies
+            phase - the phase shifts (radians)
+            error - an estimate of the error in the frequencies
+
+        Then the signal can be reconstructed with
+
+            amplitude * exp[-i * (2 pi freq t - phase) - decay t]
+        """
         self.n = signal.size
         self.signal = signal
         self.fmin = fmin
@@ -23,6 +48,8 @@ cdef class Harminv:
         self.nf = nf
 
         self.data = self.create_data()
+        self.solve()
+        self.extract_all()
 
     cdef charminv.data create_data(self):
         return charminv.data_create(self.n, <double complex *> self.signal.data,
@@ -53,19 +80,25 @@ cdef class Harminv:
         return charminv.get_freq_error(self.data, k)
 
     cpdef extract(self, thing, dtype):
+        """Extract a single output from the harminv data 
+        object. Returns an array with the given dtype.
+        """
         nf = self.get_num_freqs()
         array = np.empty((nf,), dtype)
-
         for i in range(nf):
             array[i] = getattr(self, 'get_' + thing)(i)
-
         return array
 
-    cpdef set(self):
+    cpdef extract_all(self):
+        """Extract all of the outputs from the Harminv data object."""
         self.freq = self.extract('freq', np.double)
+        self.omega = self.extract('omega', np.complex)
 
-    # cpdef np.ndarray freq(self):
-        # nf = self.get_num_freqs()
-        # # # for each nf, extract the properties and set as attributes
-        # return np.fromiter((self.get_freq(i) for i in range(nf)),
-                           # dtype=np.double)
+        self.decay = self.extract('decay', np.double)
+        self.Q = self.extract('Q', np.double)
+
+        amplitude = self.extract('amplitude', np.complex)
+        self.amplitude = np.abs(amplitude)
+        self.phase = np.angle(amplitude)
+
+        self.error = self.extract('freq_error', np.double)
--- test.py
+++ test.py
@@ -9,33 +9,5 @@ signal = np.cos(time) + np.cos(3*time)
 
 harm = harminv.Harminv(signal=signal, fmin=0.01, fmax=100, nf=100)
 
-# print dir(harm)
-print signal.size
-print harm.n
-# print harm.fmin
-# print harm.fmax
-print harm.nf
-harm.solve()
-
-print harm.get_num_freqs()
-# print harm.get_freq(0)
-# print harm.get_freq(1)
-# print harm.get_freq(2)
-
-# print harm.get_freq_error(0)
-# print harm.get_freq_error(1)
-# print harm.get_freq_error(2)
-
-# print harm.get_amplitude(0)
-# print harm.get_amplitude(1)
-# print harm.get_amplitude(2)
-
-print "extracting..."
-freq = harm.extract('freq', np.double)
-print freq
-print "extracted."
-# print harm.freq
-harm.set()
 print harm.freq
 print harm.freq[0]
-print dir(harm.freq)
