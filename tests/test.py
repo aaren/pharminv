982ee08a01f57cb5b671636477365555bda65597
Aaron O'Leary <aaron.oleary@gmail.com>
2015-03-30 07:48:19 +0100

    update tests

--- tests/test.py
+++ tests/test.py
@@ -10,13 +10,18 @@ time = np.arange(1000) * dt
 noise = 0.1 * np.random.random(1000)
 signal = noise + np.cos(tau * 0.1 * time) + np.cos(tau * 0.3 * time) + np.cos(tau * 0.5 * time)
 
-with open('input.dat', 'w') as f:
+with open('tests/input.dat', 'w') as f:
     signal.tofile(f, sep=' ')
 
-harm = harminv.Harminv(signal=signal, fmin=0.05, fmax=1, nf=100)
+harm = harminv.Harminv(signal=signal, fmin=0.001, fmax=1, nf=100)
 
-print harm.freq
-print harm.error
-# print harm.freq[0]
+# modes are accepted if Q > 10 and error < 0.1
+limit = (harm.error < 0.1) & (harm.Q > 10)
 
-# sorted = np.abs(harm.amplitude).argsort()
+for i in range(harm.freq[limit].size):
+    print("%g, %e, %g, %g, %g, %e" % (harm.freq[limit][i],
+                                      harm.decay[limit][i],
+                                      harm.Q[limit][i],
+                                      harm.amplitude[limit][i],
+                                      harm.phase[limit][i],
+                                      harm.error[limit][i]))
