dddb725978513610063931d574e4949ea2eb5792
Aaron O'Leary <aaron.oleary@gmail.com>
2015-03-31 21:39:41 +0100

    determine whether modes are valid

--- harminv/harminv.py
+++ harminv/harminv.py
@@ -1,5 +1,26 @@
+import numpy as np
+
 import _harminv
 
 
 class Harminv(_harminv.Harminv):
-    pass
+    threshold = {'error': 0.1,
+                 'relative_error': np.inf,
+                 'amplitude': 0.0,
+                 'relative_amplitude': -1.0,
+                 'Q': 10.0
+                 }
+
+    def compute_threshold(self, inrange=False):
+        rel_amp = self.amplitude.max() * self.threshold['relative_amplitude']
+        rel_err = self.error.min() * self.threshold['relative_error']
+
+        ok = (((not inrange) |
+              ((self.fmin < self.freq) & (self.fmax > self.freq)))
+              & (self.error <= self.threshold['error'])
+              & (self.error <= rel_err)
+              & (self.amplitude >= self.threshold['amplitude'])
+              & (self.amplitude >= rel_amp)
+              & (np.abs(self.Q) > self.threshold['Q']))
+
+        return ok
