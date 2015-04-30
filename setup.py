29e1786cfff7ca82ea671d2e67f0cadeeb62b096
Aaron O'Leary <aaron.oleary@gmail.com>
2015-04-30 13:57:46 +0100

    release 0.3

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if USE_CYTHON:
 
 setup(
     name='pharminv',
-    version="0.2.2",
+    version="0.3",
     description='Python interface to harminv',
     long_description=readme,
     packages=['harminv'],
